2db13453770a25f06d187bfc210b2c4551d0915f
Jacob Sanchez <sanchezjacob60@yahoo.com>
2019-09-16 16:19:53 -0500

    Project1 and Factory Method implementation

---
+++ Factory/src/main/java/FactoryHomework.java
@@ -0,0 +1,9 @@
+
+public class FactoryHomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Factory Design Pattern HW!");
+	}
+
+}
